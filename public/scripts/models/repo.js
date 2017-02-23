67c94355b1f409e5d76d1e9f3d865cd67cffdee3
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-02-22 16:36:53 -0800

    change constring and change ajax call to a jQuery get

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -7,14 +7,8 @@
 
   repos.requestRepos = function(callback) {
     // TODO: Refactor your ajax call to use the $.get method, and make a request to our new proxy route.
-    $.ajax({
-      url: `https://api.github.com/user/repos`,
-      type: 'GET',
-      headers: {'Authorization': `token ${githubToken}`}
-    })
-    .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
-    .then(callback);
-  };
+    $.get('github/user/repos')
+    .catch(error => console.log(error));
 
   repos.with = attr => repos.all.filter(repo => repo[attr]);
 
