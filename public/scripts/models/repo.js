6484a47e2e2da9d6c4a312478a81f2b18c4d90aa
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-02-22 17:11:38 -0800

    add npm install for express-request-proxy

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -8,8 +8,8 @@
   repos.requestRepos = function(callback) {
     // DONE: Refactor your ajax call to use the $.get method, and make a request to our new proxy route.
     $.get('github/user/repos')
-    .catch(error => console.log(error))
-    .then(callback);
+    .then(callback)
+    .catch(error => console.log(error));
   }
   repos.with = attr => repos.all.filter(repo => repo[attr]);
 
